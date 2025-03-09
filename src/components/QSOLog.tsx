3ff05037e8ab57233f4c9414fe5301cd0deb4d7c
Valts Silaputnins <valts@velns.org>
2025-03-09 17:33:01 +0200

    Fix case of empty

--- src/components/QSOLog.tsx
+++ src/components/QSOLog.tsx
@@ -20,7 +20,10 @@ export const QSOLog = (props: QSOLogProps) => {
   const formattedMonth =
     numericMonth < 10 ? `0${numericMonth}` : `${numericMonth}`;
 
-  const WWLs = log ? new Set(log.map((x) => x.nWWLoc4)).size : 0;
+  const WWLs = log
+    ? new Set(log.filter((val: QsoRecord) => val.nWWLoc4).map((x) => x.nWWLoc4))
+        .size
+    : 0;
 
   return (
     <>
